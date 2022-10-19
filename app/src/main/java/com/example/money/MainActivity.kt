e4ec5d335a90659e2ab83c1054471dcc96e4e2d4
David Pokol <pdavee98@gmail.com>
2022-10-19 07:55:34 +0200

    M-002 adding binding and date textview

--- app/src/main/java/com/example/money/MainActivity.kt
+++ app/src/main/java/com/example/money/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.money
 
+import android.annotation.SuppressLint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Window
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
+import com.example.money.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getSupportActionBar()?.hide()
+
+        binding.dateTextView.text = getDate()
+
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun getDate(): String {
+
+        val sdf = SimpleDateFormat("yyyy.MM.dd")
+        val currentDate = sdf.format(Date())
+
+        return currentDate;
     }
 }
